73db8ded26ac93ef376b7c4cbbe332bc3c909057
giacomoradin <giacomo.radin@gmail.com>
2026-05-13 14:39:31 +0200

    Updated roles name into user.js. We now have rigugio, groupLeader and admin. We have keept the virtualization towards the hikeSession instead of stacking on the user

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -6,7 +6,11 @@ const userSchema = new Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
+    role: {
+      type: String,
+      enum: ["groupLeader", "rifugio", "admin"],
+      default: "groupLeader",
+    },
     isVerified: { type: Boolean, default: false },
     verificationToken: { type: String },
     createdAt: { type: Date, default: Date.now },

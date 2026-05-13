16811ce2559ab0ce3b9f42b0df138679407c15f7
STUSSY-user <marcostoica4@gmail.com>
2026-05-13 11:45:53 +0200

    Middlewares folder

--- backend/src/routes/hikeSessionRoutes.js
+++ backend/src/routes/hikeSessionRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authenticate } from "../services/authMiddleware.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
 import {
   createSession,
   getSessionById,

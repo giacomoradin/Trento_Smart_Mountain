ab1f3d61d18884371fc359534d25f9113bc66993
STUSSY-user <marcostoica4@gmail.com>
2026-05-13 16:37:21 +0200

    syncing branches ig

--- backend/src/middlewares/authorizationMiddleware.js
+++
@@ -1,6 +0,0 @@
-export const requireRoles = (...allowedRoles) => (req, res, next) => {
-  if (!req.user || !allowedRoles.includes(req.user.role)) {
-    return res.status(403).json({ message: "Forbidden." });
-  }
-  next();
-};
